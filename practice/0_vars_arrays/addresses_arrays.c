6b79f6c0515a5000fca97b3aacd9a3c01fdc20c6
il-bychkov <il.bychkov@gmail.com>
2023-11-06 21:10:56 +0300

    remove constant, add define + comments

--- practice/0_vars_arrays/addresses_arrays.c
+++ practice/0_vars_arrays/addresses_arrays.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
+#define ARR_SIZE 10
+
 int main()
 {
-    const int arrLen = 10;
-    int intArray[arrLen] = {10, 11, 12, 13 ,14 ,15 ,16 ,17, 18, 19};
+	// Initialize an array
+    int intArray[ARR_SIZE] = {10, 11, 12, 13 ,14 ,15 ,16 ,17, 18, 19};
 
-    
-    for (int i = 0; i < arrLen; i++)
+    // Print an address of every array element
+    for (int i = 0; i < ARR_SIZE; i++)
         printf("int array element shift = [%d] -  value stored = %d  - address = %p\n",i, intArray[i], &intArray[i]);
     return 0;
 }
