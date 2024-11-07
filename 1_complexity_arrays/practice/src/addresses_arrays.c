5ef5bd2c1c38719e25ed6c320ad76282c48a6104
Maksim Zakharov <mttpr28@gmail.com>
2024-11-07 17:27:05 +0300

    Add practice 1

--- 1_complexity_arrays/practice/src/addresses_arrays.c
+++ 1_complexity_arrays/practice/src/addresses_arrays.c
@@ -7,7 +7,8 @@ int main() {
     int intArray[ARR_SIZE] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
 
     // Print an address of every array element
-    for (int i = 0; i < ARR_SIZE; i++)
+    for (int i = 0; i < ARR_SIZE; i++) {
         printf("int array element shift = [%d] -  value stored = %d  - address = %p\n", i, intArray[i], &intArray[i]);
+    }
     return 0;
 }
