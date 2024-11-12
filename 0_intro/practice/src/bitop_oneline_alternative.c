2d5f9e3d4e79aa0c379e603ff7125d90fd27eadc
Artiom <azhuchok207@gmail.com>
2024-11-12 09:38:58 +0300

    add alt solution task_0_practice_0

---
+++ 0_intro/practice/src/bitop_oneline_alternative.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	int x = 0b11001; //25
+	int n = 2; //bit, starts from 1
+	int mask = pow(2, n - 1);
+
+	printf("Add n bit: %d\n", mask | x);
+	printf("Remove n bit: %d\n", ~mask & x);
+	printf("Inversion n bit: %d\n", mask ^ x);
+	printf("Check n bit: %s\n", mask & x ? "yes" : "no");
+	return 0;
+}
